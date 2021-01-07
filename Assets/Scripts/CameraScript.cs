4c77557e4a136bf7f36e8301741e46a67485c337
ofekabo <76842189+ofekabo@users.noreply.github.com>
2021-01-07 16:54:17 +0200

    Fixed Camera & Colliders

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -11,6 +11,10 @@ public class CameraScript : MonoBehaviour
     [SerializeField] Vector3 offset;
     [SerializeField] private float cameraSpeed;
     private Transform bulletCloneHips;
+    
+
+    [SerializeField] private Transform volumeTrasnform;
+    [SerializeField] private float clampZOffset = 7;
     void Awake()
     {
         cannon = FindObjectOfType<CannonController>();
@@ -44,6 +48,15 @@ public class CameraScript : MonoBehaviour
 
     void Update()
     {
+        
 
+    }
+
+    public void ClampCamera()
+    {
+        float clampedY = Mathf.Clamp(transform.position.y, 7, 55);
+        float clampedZ = Mathf.Clamp(transform.position.z, 0, volumeTrasnform.position.z + clampZOffset);
+        Vector3 clampedPos = new Vector3(transform.position.x, clampedY, clampedZ);
+        transform.position = clampedPos;
     }
 }
--- Assets/Scripts/RagdollScript.cs
+++ Assets/Scripts/RagdollScript.cs
@@ -10,16 +10,25 @@ public class RagdollScript : MonoBehaviour
     private string floorTag;
 
     private bool sloMo = false;
+    private bool cameraClamp;
 
     public bool scaleTime;
     private GameManager GameManager;
+    private CameraScript _camera;
 
     // Start is called before the first frame update
     void Start()
     {
         GameManager = FindObjectOfType<GameManager>();
+        cameraClamp = false;
+        _camera = Camera.main.transform.GetComponent<CameraScript>();
+
     }
 
+    public bool GetCameraClamp()
+    {
+        return cameraClamp;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -31,13 +40,16 @@ public class RagdollScript : MonoBehaviour
         //         Time.timeScale = 1;
         //     }
         // }
+        _camera.ClampCamera();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+       
         if (other.CompareTag(floorTag))
         {
            GameManager.addScore();
+           cameraClamp = true;
         }
     }
 }
