8e2abcd6dc2f449c73be247ef1536f2e52b4824f
Tomer <tomer.c133@gamil.com>
2021-01-11 17:53:41 +0200

    rocket fix

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,46 +5,60 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    [SerializeField] private bool launched = false; 
+    [SerializeField] private bool launched = false;
     private bool launchNow = false;
     private Vector3 _flyTo;
     private Vector3 _flyToGameObject;
     private GameObject playerFoot;
     private GameObject player;
+    private bool detachPlayer;
 
 // Start is called before the first frame update
     void Start()
     {
+        detachPlayer = true;
         launched = false;
-        _flyToGameObject = transform.GetChild(0).position; 
-        _flyTo = new Vector3(_flyToGameObject.x,_flyToGameObject.y,_flyToGameObject.z);
+        _flyToGameObject = transform.GetChild(0).position;
+        _flyTo = new Vector3(_flyToGameObject.x, _flyToGameObject.y, _flyToGameObject.z);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (Vector3.Distance(transform.position, _flyTo) < 20)
+        {
+            foreach (Rigidbody bodyPart in player.GetComponentsInChildren<Rigidbody>())
+            {
+                bodyPart.AddForce(transform.forward , ForceMode.Impulse);
+            }
 
-        if (launched)
+            detachPlayer = true;
+        }
+
+
+        if (Vector3.Distance(transform.position, _flyTo) < 1)
+        {
+            launched = false;
+            launchNow = false;
+        }
+
+        if (!detachPlayer)
         {
             playerFoot.transform.position = transform.position;
             player.transform.position = transform.GetChild(1).position;
-            if(launchNow)
+        }
+
+        if (launched)
+        {
+            if (launchNow)
             {
-                transform.position = Vector3.Lerp(transform.position,_flyTo , 3f * Time.deltaTime); 
+                transform.position = Vector3.Lerp(transform.position, _flyTo, 7 * Time.deltaTime);
             }
             else
             {
                 StartCoroutine(launchSetUp());
             }
-           
         }
-            
-        if (Vector3.Distance(transform.position, _flyTo) < 10)
-        {
-            launched = false;
-            launchNow = false;
-        }
-            
     }
 
     private void OnTriggerEnter(Collider other)
@@ -52,7 +66,9 @@ public class Rocket : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             player = other.gameObject;
+            detachPlayer = false;
         }
+
         if (other.CompareTag("playerFoot"))
         {
             launched = true;
@@ -62,8 +78,7 @@ public class Rocket : MonoBehaviour
 
     IEnumerator launchSetUp()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.5f);
         launchNow = true;
-        
     }
 }
