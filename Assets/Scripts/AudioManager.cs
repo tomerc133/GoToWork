6c856ae70eeb81ffcfd2436708e40a751e9c57f5
ofekabo <76842189+ofekabo@users.noreply.github.com>
2021-01-18 16:16:01 +0200

    added tomer effects

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,14 +1,69 @@
-﻿using System;
+﻿
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private AudioSource _as;
     
-    void Awake()
+    [SerializeField] private AudioClip[] boomSounds;
+    [SerializeField] private AudioClip chargingUp;
+    [SerializeField] private AudioClip chargingDown;
+    private bool _isCupDone,_isCDownDone,_isBoomSoundDone = false;
+    public bool stopChargingSound = false;
+    
+    void Start()
     {
-        DontDestroyOnLoad(transform.gameObject);
+        _as = GetComponent<AudioSource>();
     }
+
+
+    public void PlayBoomSound()
+    {
+        if (!_isBoomSoundDone)
+        {
+            _as.Stop();
+            int rand = Random.Range(0, boomSounds.Length);
+            _as.PlayOneShot(boomSounds[rand], 0.5f);
+            _isBoomSoundDone = true;
+        }
+        
+    }
+
+    public void PlayChargingUp()
+    {
+        if (!_isCupDone)
+        {
+            _as.Stop();
+            _as.PlayOneShot(chargingUp, 1f);
+            _isCupDone = true;
+        }
+        
+    }
+    
+    public void PlayChargingDown()
+    {
+        if (!_isCDownDone)
+        {
+            _as.Stop();
+            _as.PlayOneShot(chargingDown, 1f);
+            _isCDownDone = true;
+        }
+
+    
+
+    }
+
+    public void ResetSounds()
+    {
+        _isCupDone = false;
+        _isCDownDone = false;
+        _isBoomSoundDone = false;
+    }
+
+    
 }
